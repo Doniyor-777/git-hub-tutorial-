9e8c4d9b44df708dbe576f5394ec85f9f2ffb50d
Doniyor-777 <sobirjanovdoniyor77@gmail.com>
2023-08-05 14:49:55 +0500

    main

---
+++ js/java.js
@@ -0,0 +1,143 @@
+"use strict";
+
+// let number = 10;
+// console.log(number);
+// alert("Are you ready to get started")
+
+
+// let number = 'age:'+10;
+
+// const myName = 'Name:Doniyor';
+
+// var clientName = 'client:azimbboy';
+
+// console.log(myName);
+// console.log(number);
+// console.log(clientName);
+
+    // const numbers = {
+    //     color:'blue'
+    // }
+
+
+// Data types 
+//NUMBER
+// let number = 5.6; // data type = number 
+// console.log(-8 / 0); // result = infinity 
+// console.log('doniyor' * 8); //result = NaN
+
+//STRING
+// const clientName = 'Doniyor';
+
+//BOOLEAN
+// const isMarried = true;
+// const isNotMarried = false;
+
+//Undefined 
+// let und;
+// console.log(und);
+
+//Null is often seen if you have called a variable that does not exist 
+// console.log(clienAge);
+
+//OBJECT
+
+// const thieft = {
+//  key || value
+//     age: 30,//number 
+//     jacket : "black",//string 
+//     isLong : false,//boolean
+// }
+// console.log(thieft);
+// console.log(thieft["age"]);// this way of calling constanant is also ok
+
+
+//ARRAY
+// arrays' key is their index numbers 
+//index nums    0      1        2    3  4  5 
+// let colors = ['red','yellow','green',10,[],{}]
+// console.log(colors[0]);
+
+
+// confirm returns true and false
+// const place = confirm('are you here?')
+// console.log(place);
+
+// console.log(typeof ****) checks the data type
+
+// const currentPlace = prompt("Where are you?","Doniyor")
+// console.log(typeof currentPlace);
+
+
+// const age = +prompt("How old are you?","18")
+// console.log(age + 10); // "18" + 10 == 1810 => 
+
+
+// Massivga ma'lmot qo'shish
+// const favouriteColor = []
+
+// favouriteColor[0] = 'doniyor';
+// favouriteColor[0] = prompt("What is your favourite color #1?","")
+// favouriteColor[1] = prompt("What is your favourite color #2?","")
+// favouriteColor[2] = prompt("What is your favourite color #3?","")
+
+
+// console.log(favouriteColor);
+
+
+
+// interpolation 
+
+// const channel = prompt("Who is your favourite youtuber?")
+
+// console.log(`https://youtube.com/${channel}`);
+
+
+// 2nd version
+// !!!!!  "Alert" and "Prompt" only works in browser not in VsCode !!!!!! 
+// const user = "Doniyor";
+// const job = "Full-stack Developper"
+
+// console.log(`Hello, my name is ${user},i am a ${job} `);
+
+
+//if  we are to put + in front of the string then it becomes number 
+// console.log(6 + +"9");
+
+
+//INCREMENT AND DECREMENT
+
+
+// let math = +prompt("Enter any number you want and i'll multiply it to 2x") 
+// console.log(math * 2);
+
+//IELTS CALCULATOR
+
+// const overall = []
+
+// overall[0] = +prompt("What is your Listening Score?")
+// overall[1] = +prompt("What is your Reading Score?")
+// overall[2] = +prompt("What is your Writing Score?")
+// overall[3] = +prompt("What is your speaking Score?")
+
+
+// console.log((overall[0] + overall[1] + overall[2] + overall[3]) / 4);
+
+//INCREMENT AND DECREMENT 
+// IF WE ARE TO PUT THE ++ OR -- AFTER THE ATTRIBUTE "console.log(incr++)" it needs to be run one more time and at that timer 
+//only increment works IF NOT I MEAN IF WE PUT ++ OR -- BEFORE THEM IT AUTOMATICALLY WORKS 
+
+// PERCENTAGE [%] IF WE DIVIDE IN THIS WAY console.log(5 % 2); THE ANSWER WOULD BE "1" CAUSE IT ONLY DIVIDES AND SHOWS THE LEFTOVER
+
+
+// [=] EQUAL IF console.log(5*5 == "25") = true
+// IF console.log(5*5 === "25") = false 
+
+
+
+//[&&] AND [ || ] 
+
+//&& IF INSIDE OF MORE THAN ONE VARIABLE OR ONSTANAT, THERE IS ONLY ONE FALSE IT RETURNS "FALSE"
+// || IF INSIDE OF THREE VARIABLES,THERE IS ONLY ONE TRUE OTHERS ARE FALSE THAT IT RETUERNS TRUE 
+
+//[!] it means opposite of == 
